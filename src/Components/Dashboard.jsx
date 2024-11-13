49fb744076f16c2ac7bbe569521c51e79b5f5059
QaseemB <Qbarnhardt@yahoo.com>
2024-11-13 16:55:02 -0500

    sidebar selection

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -5,11 +5,18 @@ import LinePlot from './LinePlot';
 
 export function Dashboard() {
     return (
-        <div>
+        <>
+         <div className='lineGraph'>
             <h2>DashBoard</h2>
             <LinePlot data={[1,2,3,4,5]} />
         </div>
+
+        <div className='Profile'>
+            <h2>Profile</h2>
+        </div>
+        </>
     )
 }
+
 
 export default {LinePlot}
--- src/Components/SideNavbar.jsx
+++ src/Components/SideNavbar.jsx
@@ -9,8 +9,32 @@ export const SideNav = (props) => {
 return (
     <div className={`sideNav ${collapsed ? 'collapsed' : ''}`}>
         <button onClick={handleToggleSidebar}>Toggle sidebar</button>
-        <a href="test1">Stocks</a>
+        <a href="test1">Stocks
+            <label>
+                <select>
+                    <option value="AAPL">Apple</option>
+                    <option value="TSLA">Tesla</option>
+                    <option value="GOOG">Google</option>
+                    <option value="AMZN">Amazon</option>
+                    <option value="MSFT">Microsoft</option>
+                    <option value="META">Meta</option>
+                    <option value="IBM">IBM</option>
+                    
+                </select>
+            </label>
+        </a>
         <a href="test2">Crypto</a>
+            <label>
+                <select>
+                    <option value="BTC">Bitcoin</option>
+                    <option value="DOGE"> Dogecoin</option>
+                    <option value="ETH">Ethereum</option>
+                    <option value="SOL">Solana</option>
+                    <option value="XRP">Ripple</option>
+                    <option value="SHIB">Shiba-Inu</option>
+                    <option value="USDT">Tether</option>
+                </select>
+            </label>
         <a href="#">Contact</a>
     </div>
     );
