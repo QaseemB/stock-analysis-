ab21e2f27deac724f801ad67a1983996d7729f5c
QaseemB <Qbarnhardt@yahoo.com>
2024-12-19 01:11:35 -0500

    adding port

--- stock_analysis/main.py
+++ stock_analysis/main.py
@@ -1,12 +1,18 @@
 from flask import Flask
 from app.routes import routes
+from dotenv import load_dotenv
+import os
 
 app = Flask(__name__)
 app.register_blueprint(routes)
+
+load_dotenv()
+
 
 @app.route('/')
 def hello():
     return "Hello, World!"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT_PYTHON", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
