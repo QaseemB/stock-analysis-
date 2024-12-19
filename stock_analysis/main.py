717118c065ed15722546c7b7e9e28c16395376ad
QaseemB <Qbarnhardt@yahoo.com>
2024-12-19 01:30:11 -0500

    fixing port again

--- stock_analysis/main.py
+++ stock_analysis/main.py
@@ -14,5 +14,5 @@ def hello():
     return "Hello, World!"
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT_PYTHON", 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    port =  os.getenv("PORT", 5000)
+    app.run(debug=True, host='0.0.0.0', port=int(port))
