af9dc8bc964fa2cfe51040b5d7a280699c89ea09
QaseemB <Qbarnhardt@yahoo.com>
2024-12-19 00:50:46 -0500

    fix application name in procfile

--- stock_analysis/main.py
+++ stock_analysis/main.py
@@ -4,5 +4,9 @@ from app.routes import routes
 app = Flask(__name__)
 app.register_blueprint(routes)
 
+@app.route('/')
+def hello():
+    return "Hello, World!"
+
 if __name__ == "__main__":
     app.run(debug=True)
