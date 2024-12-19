a0205802771af7dc23d5ac7d33b42386a6b2af78
QaseemB <Qbarnhardt@yahoo.com>
2024-12-19 10:21:58 -0500

    adding cors

--- stock_analysis/main.py
+++ stock_analysis/main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 from app.routes import routes
 from dotenv import load_dotenv
 import os
@@ -7,6 +8,9 @@ app = Flask(__name__)
 app.register_blueprint(routes)
 
 load_dotenv()
+# Allow specific origins
+
+CORS(app, resources={r"/*": {"origins": "https://stock-analysis-0f8t.onrender.com"}})
 
 
 @app.route('/')
