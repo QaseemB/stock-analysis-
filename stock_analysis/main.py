52fd53f68375738472e0945188cd663e2b7de8cf
QaseemB <Qbarnhardt@yahoo.com>
2024-12-19 01:46:26 -0500

    changed env variable

--- stock_analysis/main.py
+++ stock_analysis/main.py
@@ -14,5 +14,5 @@ def hello():
     return "Hello, World!"
 
 if __name__ == "__main__":
-    port =  os.getenv("PORT", 5000)
-    app.run(debug=True, host='0.0.0.0', port=int(port))
+    port =  os.getenv("PORT_PYTHON", 5000)
+    app.run(debug=True, host='0.0.0.0', port=int(port)+ 1 )
