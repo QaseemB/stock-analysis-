c69623f898c0ee83af27ff8b23b6bf91c44a280a
QaseemB <Qbarnhardt@yahoo.com>
2025-02-22 16:03:37 -0500

    processed data back up to s3

---
+++ ProcessedDataBackup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Load environment variables
+source /path/to/your/.env
+
+# Variables from .env
+DB_NAME=$SQL_NAME
+BUCKET_NAME=$S3_BUCKET
+
+# Define backup filename
+BACKUP_FILE="processedDataBackup_$(date +%Y%m%d_%H%M%S).sql"
+
+
+# Create a backup
+pg_dump -U $SQL_USER -h $SQL_HOST $SQL_NAME > $BACKUP_FILE
+
+
+# Compress the backup
+gzip $BACKUP_FILE
+
+# Upload to S3
+aws s3 cp "$BACKUP_FILE.gz" s3://$BUCKET_NAME/
+
+echo "Backup completed successfully and uploaded to S3"
